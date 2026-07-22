c088e1ffd803af93d49aac4a02cf364c0058e5f5
Alexander Pastana <alexander.pastana@gmail.com>
2026-07-22 19:29:30 -0300

    build: setup project deployment configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -8,15 +7,14 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        entry: "server",
-      },
-    }),
     react(),
   ],
   server: {
     host: "0.0.0.0",
     port: 5173,
   },
+  build: {
+    // Garante que o Vite vai buildar o app focado no lado do cliente (SPA)
+    ssr: false,
+  }
 });
